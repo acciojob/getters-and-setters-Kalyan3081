911fffc624d28318ee4ad298ad27771903ee5ec3
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-10-18 12:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-//complete this code
-class Person {}
+class Person {
+    // Constructor to initialize name and age
+    constructor(name, age) {
+        this._name = name;  // Using an underscore to indicate a private property
+        this._age = age;
+    }
 
-class Student extends Person {}
+    // Getter for the name property
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    // Setter for the age property
+    set age(newAge) {
+        if (typeof newAge === 'number' && newAge > 0) {
+            this._age = newAge;
+        } else {
+            console.error('Please provide a valid age.');
+        }
+    }
+
+    // Getter for the age property (optional, for checking age)
+    get age() {
+        return this._age;
+    }
+}
+
+// Student class that extends the Person class
+class Student extends Person {
+    // Method specific to Student class
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+// Teacher class that extends the Person class
+class Teacher extends Person {
+    // Method specific to Teacher class
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
